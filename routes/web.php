549e3992d7da0f3608e4c49b687bbef93787577b
pabloandresreyes2004@gmail.com <apinf@SERVIASESORIA.LOCAL>
2023-10-27 11:21:33 -0500

    migraciones

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ComunaController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,6 +26,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //COMUNAS
+    Route::get('/comunas',[ComunaController::class,'index'])->name('comunas.index');
+    Route::post('/comunas', [ComunaController::class,'store'])->name('comunas.store');
+    Route::get('/comunas/create',[ComunaController::class,'create'])->name('comunas.create');
+    Route::delete('/comunas/{comuna}',[ComunaController::class,'destroy'])->name('comunas.destroy');
+    Route::put('/comunas/{comuna}',[ComunaController::class,'update'])->name('comunas.update');
+    Route::get('/comunas/{comuna}/edit',[ComunaController::class,'edit'])->name('comunas.edit');
+
 });
 
 require __DIR__.'/auth.php';
